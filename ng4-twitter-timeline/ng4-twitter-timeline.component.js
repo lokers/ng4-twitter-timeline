dcca0023ae7c76b669a1959d59bef71485291ce8
Lukasz Goslawski <lokers@gmail.com>
2017-07-27 08:50:52 +0200

    fixes

---
+++ ng4-twitter-timeline/ng4-twitter-timeline.component.js
@@ -0,0 +1,12 @@
+class Ng4TwitterTimelineController {
+    constructor() {
+    }
+}
+
+export default {
+    controller: Ng4TwitterTimelineController,
+    template: require('./ng4-twitter-timeline.html'),
+    bindings: {
+        
+    }
+};
